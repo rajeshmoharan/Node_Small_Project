dbccf72e43e591047bda2f432f9e2ea2c3fe6b06
rajeshmoharan <rajeshmoharana230@gmail.com>
2023-07-25 13:12:24 +0530

    Node js code push

---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const requestHandler = (req,res) =>{
+    const method = req.method;
+
+    if(req.url === '/'){ 
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+         res.end();
+    }
+    if (req.url === '/message' && method === "POST"){
+        const body = [];
+        req.on('data',(chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end',() =>{
+            const dataSet = Buffer.concat(body).toString();
+            console.log(dataSet);
+            const newData = dataSet.split('=')[1];
+            fs.writeFile('input.txt',newData, (err) =>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            })
+        })
+        
+    }
+}
+
+// Export 
+module.exports = requestHandler;
+
+// we can do export in differnet way
+// module.exports = {
+//     handler : requestHandler
+// }
+
+// one way to export the module
+// module.exports.handler = requestHandler;
